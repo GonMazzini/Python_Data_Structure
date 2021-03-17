e4727617433f807e54352ec8feda1b7b0af07ca8
GonMazzini <gmazzini@itba.edu.ar>
2021-03-17 19:04:47 -0300

    add array

---
+++ Array.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 17:23:31 2021
+
+@author: Gonzalo
+"""
+
+class MyArray():
+    
+    def __init__(self):
+        self.length = 0
+        self.data = {}
+        
+    def __str__ 
+        
+    def get(self, index):
+        
+        return self.data[index]
+    
+    def push(self, item):
+        self.data[self.]
+    
+# %%
+
+    
+newArray = MyArray()
+try:
+    print(newArray.get(0))     
+except IndexError as err:
+    print(err)
+    
+
+# %%
--- Python advance.py
+++ Python advance.py
@@ -16,16 +16,40 @@ class Movil():
         self.wheels = wheels
         self.gates = gates
         
+    
+    def __str__(self):
+        return "MyClass(wheels" + " " +  str(self.wheels)
+    
+    @classmethod
     def info(cls):
-        if start == True:
+        if cls.start == True:
             status = "Start"
         else:
             status = "Stop"
         
-        return print(f"this movil status is {status}")
-        
-        
+        return print(f"this movil status is: {status}")
 
+
+Movil.info()  # calling a class method
+
+car = Movil(wheels=4, gates=3) # instanciate a movil object
+
+# %%
+
+# Here all three give the same output, which is the pointer of our object. 
+# When __str__, print(), or str() are called you will get your defined output
+#. Make note that the __repr__ output remains the same.
+
+print(car.__str__())
+print(car.__repr__())
+print(car)
+
+
+# %%
+print(dir(Movil))
+print(dir(car)) # same but also wheels and gates.
+# %%
+print(type(car))
 
 
 # %%
